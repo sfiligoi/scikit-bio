5384d0fc69ee22266ed317652eeb95957570cb5d
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-01-27 08:50:36 -0800

    Add fma wrappers

--- skbio/stats/distance/_utils.py
+++ skbio/stats/distance/_utils.py
@@ -10,23 +10,6 @@ import numpy as np
 
 from ._cutils import vec_fma_cy, distmat_reorder_cy, distmat_reorder_condensed_cy
 
-def vec_fma_buf(in_vec, mul, add, out_vec):
-    """
-    Compute out_vec = mul*in_vec + add
-
-    Parameters
-    ----------
-    in_mat : 1D array_like
-        Input array
-    mul: real
-        Multiplication factor
-    add: real
-        Addition
-    out_mat : 1D array_like
-        Output array, safe to use in_vec, too
-    """
-    vec_fma_cy(in_vec, mul, add, out_vec)
-
 def vec_fma(in_vec, mul, add):
     """
     Compute out_vec = mul*in_vec + add
@@ -48,50 +31,6 @@ def vec_fma(in_vec, mul, add):
     out_vec = np.empty(in_vec.shape, in_vec.dtype)
     vec_fma_cy(in_vec, mul, add, out_vec)
     return out_vec
-
-def distmat_reorder_buf(in_mat, reorder_vec, out_mat, validate=False):
-    """
-    Reorder the rows and columns of a distance matrix
-    given a reorder vector.
-    Not all of the columns need to be used.
-
-    For example:
-     [ [0, 1, 2, 3] ,
-       [1, 0, 4, 5] ,
-       [2, 4, 0, 6] ,
-       [3, 5, 6, 0] ]
-     with
-     [1,0,3,2]
-     will result in
-     [ [0, 1, 5, 4] ,
-       [1, 0, 3, 2] ,
-       [5, 3, 0, 6] ,
-       [4, 2, 6, 0] ]
-
-    Parameters
-    ----------
-    in_mat : 2D array_like
-        Distance matrix
-    reorder_vec : 1D_array_like
-        List of permutation indexes
-    out_mat : 2D array_like
-        Output, Distance matrix,
-        must be in c_order and same size as reorder_vec
-    validate: boolean
-        Optional, if True, validate reorder_vec content, detaults to False
-    """
-    np_reorder = np.asarray(reorder_vec, dtype=np.long)
-    if validate:
-        maxsize = in_mat.shape[0]
-        bad_cnt = np.where((np_reorder < 0) or (np_reorder >= maxsize))[0].size
-        if bad_cnt > 0:
-            raise ValueError("Invalid reorder_vec")
-
-    if not in_mat.flags.c_contiguous:
-        in_mat = np.asarray(in_mat, order='C')
-
-    distmat_reorder_cy(in_mat, np_reorder, out_mat)
-
 
 def distmat_reorder(in_mat, reorder_vec, validate=False):
     """
@@ -182,6 +121,105 @@ def distmat_reorder_condensed(in_mat, reorder_vec, validate=False):
 
     out_mat_condensed = np.empty([np.long(((np_reorder.size-1)*np_reorder.size)/2)], in_mat.dtype)
     distmat_reorder_condensed_cy(in_mat, np_reorder, out_mat_condensed)
+    return out_mat_condensed
+
+def distmat_reorder_fma(in_mat, reorder_vec, mul, add, validate=False):
+    """
+    Reorder the rows and columns of a distance matrix
+    given a reorder vector, and perform FMA on it (mul*val+add).
+    Not all of the columns need to be used.
+
+    For example:
+     [ [0, 1, 2, 3] ,
+       [1, 0, 4, 5] ,
+       [2, 4, 0, 6] ,
+       [3, 5, 6, 0] ]
+     with
+     [1,0,3,2], 3, 1
+     will result in
+     [ [ 0, 2, 14, 11] ,
+       [ 2, 0,  8,  5] ,
+       [14, 8,  0, 17] ,
+       [11, 5, 17,  0] ]
+
+    Parameters
+    ----------
+    in_mat : 2D array_like
+        Distance matrix, must be in c_order
+    reorder_vec : 1D_array_like
+        List of permutation indexes
+    mul: real
+        Multiplication factor
+    add: real
+        Addition
+    validate: boolean
+        Optional, if True, validate reorder_vec content, detaults to False
+
+    Returns
+    -------
+    out_mat : 2D array_like
+        Distance matrix
+    """
+    np_reorder = np.asarray(reorder_vec, dtype=np.long)
+    if validate:
+        maxsize = in_mat.shape[0]
+        bad_cnt = np.where((np_reorder < 0) or (np_reorder >= maxsize))[0].size
+        if bad_cnt > 0:
+            raise ValueError("Invalid reorder_vec")
+
+    if not in_mat.flags.c_contiguous:
+        in_mat = np.asarray(in_mat, order='C')
+
+    out_mat = np.empty([np_reorder.size, np_reorder.size], in_mat.dtype)
+    distmat_reorder_fma_cy(in_mat, np_reorder, mull, add, out_mat)
+    return out_mat
+
+def distmat_reorder_condensed(in_mat, reorder_vec, mul, add, validate=False):
+    """
+    Reorder the rows and columns of a distance matrix
+    given a reorder vector, and perform FMA on it (mul*val+add).
+    Not all of the columns need to be used.
+
+    For example:
+     [ [0, 1, 2, 3] ,
+       [1, 0, 4, 5] ,
+       [2, 4, 0, 6] ,
+       [3, 5, 6, 0] ]
+     with
+     [1,0,3,2], 3, 1
+     will result in
+     [ 2, 14, 11 , 8, 5, 17 ]
+
+    Parameters
+    ----------
+    in_mat : 2D array_like
+        Distance matrix, must be in c_order
+    reorder_vec : 1D_array_like
+        List of permutation indexes
+    mul: real
+        Multiplication factor
+    add: real
+        Addition
+    validate: boolean
+        Optional, if True, validate reorder_vec content, detaults to False
+
+    Returns
+    -------
+    out_mat_condensed : 1D array_like
+        Condensed distance matrix
+    """
+    np_reorder = np.asarray(reorder_vec, dtype=np.long)
+    if validate:
+        maxsize = in_mat.shape[0]
+        bad_cnt = np.where((np_reorder < 0) or (np_reorder >= maxsize))[0].size
+        if bad_cnt > 0:
+            raise ValueError("Invalid reorder_vec")
+
+    if not in_mat.flags.c_contiguous:
+        in_mat = np.asarray(in_mat, order='C')
+
+    out_mat_condensed = np.empty([np.long(((np_reorder.size-1)*np_reorder.size)/2)], in_mat.dtype)
+    distmat_reorder_condensed_fma_cy(in_mat, np_reorder, mull, add, out_mat_condensed)
     return out_mat_condensed
 
 class PearsonPermuttable:
