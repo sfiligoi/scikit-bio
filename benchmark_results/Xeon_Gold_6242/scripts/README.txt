8f3dac0c3ae3e4fd26a34c8e8e77ca003c8f3a7e
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-02-12 12:27:04 -0800

    Add scripts

---
+++ benchmark_results/Xeon_Gold_6242/scripts/README.txt
@@ -0,0 +1,3 @@
+Twoo test scripts:
+mandel1_300k_param.py - Tests mantel pearson
+mandels_300k_param.py - Trsts mantel spearman
---
+++ benchmark_results/Xeon_Gold_6242/scripts/mandels_300k_param.py
@@ -0,0 +1,29 @@
+import h5py
+import skbio
+
+import time
+
+import sys
+
+slice=int(sys.argv[1])
+print("Proessing %i * %i matrix"%(slice,slice))
+
+t1=time.time()
+with h5py.File("v300k_uw32.h5","r") as f_u:
+ dm_u=skbio.stats.distance.DistanceMatrix(f_u['matrix'][:slice,:slice],f_u['order'][:slice])
+
+with h5py.File("v300k_wn32.h5","r") as f_u:
+ dm_w=skbio.stats.distance.DistanceMatrix(f_u['matrix'][:slice,:slice],f_u['order'][:slice])
+
+
+t2=time.time()
+print("H5 %.2f"%(t2-t1))
+
+
+# example computing a mantel test
+t1=time.time()
+r, p, n = skbio.stats.distance.mantel(dm_u, dm_w, method='spearman')
+t2=time.time()
+print("MT %.2f"%(t2-t1))
+
+print(r, p, n)
