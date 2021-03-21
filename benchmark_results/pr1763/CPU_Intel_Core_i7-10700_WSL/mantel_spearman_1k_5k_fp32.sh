5bb88b317d07f3b16b5e338102ad6a615a50463a
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-03-21 15:22:07 -0700

    Add benchmark_results/pr1763/CPU_Intel_Core_i7-10700_WSL

---
+++ benchmark_results/pr1763/CPU_Intel_Core_i7-10700_WSL/mantel_spearman_1k_5k_fp32.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+hostname
+lscpu
+date
+
+export OMP_NUM_THREADS=8
+
+source ~/setup_conda.source
+conda activate skbio-local
+
+for i in 1 2 5; do 
+taskset -c 0-7 time python ../../scripts/mantel_spearman.py "${i}000 fp32" ../../inputs/unifrac_${i}000_pcoa_10.h5 ../../inputs/unifrac_${i}000_nf_pcoa_0.h5
+done
