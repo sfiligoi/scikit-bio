3833345d06fdf5ed6576817a27a3dc2fa714a762
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-03-21 12:06:01 -0700

    Fix comment

--- benchmark_results/org/CPU_Intel_Core_i7-10700_WSL/mantel_1k_10k_fp64.sh
+++ benchmark_results/org/CPU_Intel_Core_i7-10700_WSL/mantel_1k_10k_fp64.sh
@@ -9,5 +9,5 @@ source ~/setup_conda.source
 conda activate unifrac-202
 
 for i in 1 2 5 10; do 
-taskset -c 0-7 time python ../../scripts/mantel.py "${i}000 fp32" ../../inputs/unifrac_${i}000_pcoa_10.fp64.h5 ../../inputs/unifrac_${i}000_nf_pcoa_0.fp64.h5
+taskset -c 0-7 time python ../../scripts/mantel.py "${i}000 fp64" ../../inputs/unifrac_${i}000_pcoa_10.fp64.h5 ../../inputs/unifrac_${i}000_nf_pcoa_0.fp64.h5
 done
