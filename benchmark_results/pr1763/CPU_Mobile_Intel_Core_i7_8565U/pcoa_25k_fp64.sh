c3154f22bbdba15f9ed540a2170255a6e0d4dc23
isfiligoi@sdsc.edu <isfiligoi@sdsc.edu>
2021-03-22 10:15:03 -0700

    Add more benchmark results

---
+++ benchmark_results/pr1763/CPU_Mobile_Intel_Core_i7_8565U/pcoa_25k_fp64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+hostname
+lscpu
+date
+
+export OMP_NUM_THREADS=4
+
+source ~/unifrac/conda_setup.sh
+conda activate skbio-local
+
+for i in 25; do 
+taskset -c 0-3 time python ../../scripts/pcoa.py "${i}000 fp64" ../../inputs/unifrac_${i}000_pcoa_10.fp64.h5 
+done
