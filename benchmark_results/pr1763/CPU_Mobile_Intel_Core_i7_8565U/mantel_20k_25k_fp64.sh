480912dff6eb26d31723676f75f63a5b4a73dc89
isfiligoi@sdsc.edu <isfiligoi@sdsc.edu>
2021-03-22 12:01:30 -0700

    Add more benchmark results

---
+++ benchmark_results/pr1763/CPU_Mobile_Intel_Core_i7_8565U/mantel_20k_25k_fp64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+hostname
+lscpu
+date
+
+export OMP_NUM_THREADS=4
+
+source ~/unifrac/conda_setup.sh
+conda activate skbio-local
+
+for i in 20 25; do 
+taskset -c 0-4 time python ../../scripts/mantel.py "${i}000 fp64" ../../inputs/unifrac_${i}000_pcoa_10.fp64.h5 ../../inputs/unifrac_${i}000_nf_pcoa_0.fp64.h5
+done
