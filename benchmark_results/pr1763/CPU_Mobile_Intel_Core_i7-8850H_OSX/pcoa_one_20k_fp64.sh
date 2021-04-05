c63de9d976ff09c5a39fc030d23540556c8d62a5
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-04-05 09:54:35 -0700

    Add more results

---
+++ benchmark_results/pr1763/CPU_Mobile_Intel_Core_i7-8850H_OSX/pcoa_one_20k_fp64.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+hostname
+sysctl -n machdep.cpu -N |head -20
+date
+
+export OMP_NUM_THREADS=6
+
+source ../../../ana3-skbio.source
+conda activate skbio-local
+
+for i in 20 ; do 
+/usr/bin/time -l python ../../scripts/pcoa_load_only.py "${i}000 fp64" ../../inputs/unifrac_${i}000_pcoa_10.fp64.h5
+/usr/bin/time -l python ../../scripts/pcoa_inline.py "${i}000 fp64" ../../inputs/unifrac_${i}000_pcoa_10.fp64.h5
+/usr/bin/time -l python ../../scripts/pcoa_one.py "${i}000 fp64" ../../inputs/unifrac_${i}000_pcoa_10.fp64.h5
+done
